2638c51ecc9732297783315085671b94336e5311
admina347 <it-mactep@mail.ru>
2023-04-20 11:11:30 +0500

    Добавьте файлы проекта.

---
+++ HomeApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using HomeApp.Pages;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HomeApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new LoginPage();  //LoadingPage(); //MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
